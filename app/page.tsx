99fe2558b84f00db3b9cf2b0b1edae199ff80199
Johan Riascos <desarrollador7777@gmail.com>
2024-08-01 12:57:01 -0500

    NEXTJS: Cambios en estilo del navbar

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Hero from '@/components/Hero';
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
-import { FaHome, FaUser } from "react-icons/fa";
+import { FaHome } from "react-icons/fa";
 import Timeline from "@/components/ui/Timeline"; 
 import Footer from '@/components/Footer';
 
@@ -11,8 +11,7 @@ export default function Home() {
     <main className="relative bg-black-100 flex justify-center text-white items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div>
         <FloatingNav navItems={[
-          { name: 'Home', link: '#home', icon: <FaHome /> },
-          { name: 'Sobre mí', link: '#about', icon: <FaUser /> }
+          { name: '', link: '#home', icon: <FaHome size={30} /> }
         ]} />
         <section id="home">
           <Hero />
